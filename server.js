da459d880d03781253141a4b1e433fa3dc63bbd3
guoguo2827 <guoguo2827@users.noreply.github.com>
2026-07-07 00:18:02 +0800

    Load environment configuration

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const crypto = require("crypto");
 const fs = require("fs");
 const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 const express = require("express");
 const session = require("express-session");
 const bcrypt = require("bcryptjs");
